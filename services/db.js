be4d7a6fdcc47e6043f8de4366a14fa48c89b89e
Manvendra Priyadarshi <manvendra2215@gmail.com>
2019-03-10 15:36:54 +0530

    Update db.js

--- services/db.js
+++ services/db.js
@@ -1,23 +1,38 @@
 var mysql = require('mysql');
 
-var con = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "root",
-  database: "plivo_contacts"
-});
+var con;
 
-con.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
-});
+function handleDisconnect() {
+	con = mysql.createConnection({
+	  host: "localhost",
+	  user: "root",
+	  password: "root",
+	  database: "contacts_app"
+	});
 
-var createContactTable = "CREATE TABLE IF NOT EXISTS contact_list (`id` int(20) NOT NULL AUTO_INCREMENT, `name` varchar(128) DEFAULT NULL, `email_id` varchar(128) DEFAULT NULL, PRIMARY KEY (`id`), UNIQUE KEY `no_duplicate` (`email_id`)) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1";
+	con.connect(function(err) {
+	  if (err) throw err;
+	  console.log("Connected!");
+	});
 
-con.query(createContactTable, function(err, rows, fields)
-{
-	if(err)  throw err;
-	console.log("Table created!");
-});
+	var createContactTable = "CREATE TABLE IF NOT EXISTS contact_list (`id` int(20) NOT NULL AUTO_INCREMENT, `name` varchar(128) DEFAULT NULL, `email_id` varchar(128) DEFAULT NULL, PRIMARY KEY (`id`), UNIQUE KEY `no_duplicate` (`email_id`)) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1";
+
+	con.query(createContactTable, function(err, rows, fields)
+	{
+		if(err)  throw err;
+		console.log("Table created!");
+	});
+
+	con.on('error', function(err) {
+		console.log('db error', err);
+		if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+		  handleDisconnect();                         // lost due to either server restart, or a
+		} else {                                      // connnection idle timeout (the wait_timeout
+		  throw err;                                  // server variable configures this)
+		}
+	});
+}
+
+handleDisconnect();  
 
 module.exports.con = con;
